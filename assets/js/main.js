57dc7b65b0243f8c7bb75cf38a4db409d3ab163c
Bhubon Sutradhar Nil <54433969+bhubon@users.noreply.github.com>
2023-06-21 21:35:05 +0600

    Complete weather app using js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,41 +1,87 @@
-let result = document.getElementById('result');
-let searchBtn = document.getElementById('search-btn');
-let city = document.getElementById('city');
+const result = document.getElementById('result');
+const searchBtn = document.getElementById('search-btn');
+const cityInput = document.getElementById('city');
+
+const cityName = document.querySelector('.weather-city');
+const sky = document.querySelector('.description');
+const skyDetails = document.querySelector('.description-details');
+const weatherIcon = document.querySelector('.result-wrapper img');
+const temp = document.querySelector('.result-wrapper .temp');
+const min_temp = document.querySelector('.result-wrapper .details-temp.min');
+const max_temp = document.querySelector('.result-wrapper .details-temp.max');
+const feels_like = document.querySelector('.result-wrapper .details-temp.feels-like');
+const error = document.querySelector('.error');
 
 
-let cityName = document.querySelector('.weather-city');
-let sky = document.querySelector('.description');
-let weatherIcon = document.querySelector('.result-wrapper img');
-let temp = document.querySelector('.result-wrapper .temp');
-let min_temp = document.querySelector('.result-wrapper .details-temp.min');
-let max_temp = document.querySelector('.result-wrapper .details-temp.max');
-
-
-let getWeather = () => {
-    let cityValue = city.value;
+/**
+ * Init the weather using openweathermap api
+ */
+const getWeather = () => {
+    const cityValue = cityInput.value;
     if (cityValue.length > 0) {
-        let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=b4336e17ec9ff6998276f592154ffa10`;
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=b4336e17ec9ff6998276f592154ffa10`;
 
         fetch(url)
             .then((response) => response.json())
             .then((data) => {
-                console.log(data)
-                console.log(data.weather[0].icon)
-                console.log(data.weather[0].main)
-                console.log(data.weather[0].description)
-                console.log(data.name)
-                console.log(data.main)
-                console.log(data.main.temp_min)
-                console.log(data.main.temp_max)
+                setError(data)
+                assignWeather(data);
 
-                cityName.innerHTML = data.name;
-                sky.innerHTML = data.weather[0].main;
-                weatherIcon.src = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-                temp.innerHTML = data.main.temp;
-                min_temp.innerHTML = data.main.temp_min;
-                max_temp.innerHTML = data.main.temp_max;
             })
+            .catch(error => {
+                setError()
+                assignWeather()
+
+            })
+    } else {
+        setError()
+        assignWeather()
     }
 }
+
 searchBtn.addEventListener('click', getWeather);
 window.addEventListener('load', getWeather);
+
+/**
+ * Convert farenheit to celcius
+ * @param int temperature 
+ * @returns int
+ */
+const convertToCelsius = (temperature) => (temperature - 273.15).toFixed(2);
+
+/**
+ * Assign the weather fetch data innto html 
+ * @param {object} data 
+ */
+const assignWeather = (data) => {
+    if (data) {
+        cityName.innerHTML = data.name
+        sky.innerHTML = data.weather[0].main
+        skyDetails.innerHTML = data.weather[0].description
+        weatherIcon.src = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`
+        temp.innerHTML = `${convertToCelsius(data.main.temp)}&#176;`
+        min_temp.innerHTML = `${convertToCelsius(data.main.temp_min)}&#176;`
+        max_temp.innerHTML = `${convertToCelsius(data.main.temp_max)}&#176;`
+        feels_like.innerHTML = `${convertToCelsius(data.main.feels_like)}&#176;`
+    } else {
+        setError()
+    }
+}
+
+/**
+ * Handle the errors
+ * @param {object} data 
+ */
+const setError = (data) => {
+    if (!data) {
+        result.style.display = 'none'
+        if (cityInput.value == '') {
+            error.innerHTML = 'Please enter a city name'
+        } else {
+            error.innerHTML = 'Not a valid city name'
+        }
+    } else {
+        result.style.display = 'block'
+        error.innerHTML = ''
+    }
+}
